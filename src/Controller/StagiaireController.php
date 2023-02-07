67296010b210f320f21fca22a07d1a46368fc7be
YulyBulatov <YulyBulatov>
2023-02-07 16:54:08 +0100

    List stagiaires implemented. working on stagiaire

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\StagiaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StagiaireController extends AbstractController
 {
-    #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
+    #[Route('/stagiaires', name: 'app_stagiaire')]
+    public function index(StagiaireRepository $stagiaireRepository): Response
     {
         return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
+            'stagiaires' => $stagiaireRepository->findAll()
+        ]);
+    }
+
+    #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
+    public function showStagiaire($id,StagiaireRepository $stagiaireRepository ): Response
+    {
+        return $this->render('stagiaire/show.html.twig', [
+            'stagiaire' => $stagiaireRepository->findOneBy($id)
         ]);
     }
 }
