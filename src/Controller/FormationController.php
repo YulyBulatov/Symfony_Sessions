51970e713e85d682bcd591c6a4de47c68e55f4d6
YulyBulatov <yulik.bulatov@gmail.com>
2023-02-13 10:31:27 +0100

    list formations implemented

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class FormationController extends AbstractController
 {
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formationRepository->findAll(),
         ]);
     }
 }
